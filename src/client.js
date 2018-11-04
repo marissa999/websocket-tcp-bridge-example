715d5c5c711fbd1a23a19c95802b9ba4fd4ebb0d
Marissa <8284445+marissa999@users.noreply.github.com>
2018-11-04 18:17:52 +0100

    cleaned up code

--- src/client.js
+++ src/client.js
@@ -1,41 +1,61 @@
 let connection;
 
-document.getElementById('settingsForm').addEventListener('submit', (event) => {
+document.getElementById("settingsForm").addEventListener(
+  "submit",
+  event => {
     event.preventDefault();
 
-    const middlewareHostname = document.getElementById('settingsMiddlewareHostname').value;
-    const middlewarePort = document.getElementById('settingsMiddlewarePort').value;
-    const endpointHostname = document.getElementById('settingsEndpointHostname').value;
-    const endpointPort = document.getElementById('settingsEndpointPort').value;
+    const middlewareHostname = document.getElementById(
+      "settingsMiddlewareHostname"
+    ).value;
+    const middlewarePort = document.getElementById("settingsMiddlewarePort")
+      .value;
+    const endpointHostname = document.getElementById("settingsEndpointHostname")
+      .value;
+    const endpointPort = document.getElementById("settingsEndpointPort").value;
 
     // if a field is empty do nothing
-    if (!middlewareHostname ||
-        !middlewarePort ||
-        !endpointHostname ||
-        !endpointPort){
-        console.log("At least one field is not defined!");
-        return;
+    if (
+      !middlewareHostname ||
+      !middlewarePort ||
+      !endpointHostname ||
+      !endpointPort
+    ) {
+      console.log("At least one field is not defined!");
+      return;
     }
 
     // connect to server
-    connection = new MiddleWareConnect(middlewareHostname, middlewarePort, endpointHostname, endpointPort, (data) => {
+    connection = new MiddleWareConnect(
+      middlewareHostname,
+      middlewarePort,
+      endpointHostname,
+      endpointPort,
+      data => {
         addMessageToLog("server", data);
-    });    
-}, true);
+      }
+    );
+  },
+  true
+);
 
-document.getElementById('connectionForm').addEventListener('submit', (event) => {
+document.getElementById("connectionForm").addEventListener(
+  "submit",
+  event => {
     event.preventDefault();
-    data = document.getElementById('sendMessageText').value + "\r\n";
+    const data = document.getElementById("sendMessageText").value + "\r\n";
     connection.sendMessage(data);
     addMessageToLog("client", data);
-}, true);
+  },
+  true
+);
 
-function addMessageToLog(from, message){
-    const textLog = document.getElementById('logs');
-    const d = new Date();
-    const hh = d.getHours() < 10 ? "0" + d.getHours() : d.getHours();
-    const mm = d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes();
-    const ss = d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds();
-    textLog.value += `${hh}:${mm}:${ss} - ${from}: ${message}\r\n`;
-    textLog.scrollTop = textLog.scrollHeight
+function addMessageToLog(from, message) {
+  const textLog = document.getElementById("logs");
+  const d = new Date();
+  const hh = d.getHours() < 10 ? "0" + d.getHours() : d.getHours();
+  const mm = d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes();
+  const ss = d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds();
+  textLog.value += `${hh}:${mm}:${ss} - ${from}: ${message}\r\n`;
+  textLog.scrollTop = textLog.scrollHeight;
 }
--- src/middlewareConnect.js
+++ src/middlewareConnect.js
@@ -1,6 +1,5 @@
-class MiddleWareConnect{
-
-	/* properties:
+class MiddleWareConnect {
+  /* properties:
 		this.socket;
 		this.sendMessage;
 		this.settingsMiddleware = {
@@ -13,56 +12,59 @@ class MiddleWareConnect{
 		};
 	*/
 
-	constructor(middlewareHostname, middlewarePort, endPointHostname, endPointPort, receiveMessage){
-		// init all properties
-		this._socket;
-		this._receiveMessage;
-		this._settingsMiddleware;
-		this._settingsEndpoint;
+  constructor(
+    middlewareHostname,
+    middlewarePort,
+    endPointHostname,
+    endPointPort,
+    receiveMessage
+  ) {
+    // init all properties
+    this._socket;
+    this._receiveMessage;
+    this._settingsMiddleware;
+    this._settingsEndpoint;
 
-		this._settingsMiddleware = {
-			'hostname': null,
-			'port': null,
-		};
+    this._settingsMiddleware = {
+      hostname: null,
+      port: null
+    };
 
-		this._settingsEndpoint = {
-			'hostname': null,
-			'port': null,
-		};
+    this._settingsEndpoint = {
+      hostname: null,
+      port: null
+    };
 
-		this._receiveMessage = receiveMessage;
+    this._receiveMessage = receiveMessage;
 
-		// assign middleware settings
-		this._settingsMiddleware.hostname = middlewareHostname;
-		this._settingsMiddleware.port = middlewarePort;
+    // assign middleware settings
+    this._settingsMiddleware.hostname = middlewareHostname;
+    this._settingsMiddleware.port = middlewarePort;
 
-		// assign endPoint settings
-		this._settingsEndpoint.hostname = endPointHostname;
-		this._settingsEndpoint.port = endPointPort;
+    // assign endPoint settings
+    this._settingsEndpoint.hostname = endPointHostname;
+    this._settingsEndpoint.port = endPointPort;
 
-		// print for debug
-		console.log(`Middleware settings:\n\r${JSON.stringify(this._settingsMiddleware, null, 2)}`);
-		console.log(`Endpoint settings:\n\r${JSON.stringify(this._settingsEndpoint, null, 2)}`);
+    this._socket = io(
+      this._settingsMiddleware.hostname + ":" + this._settingsMiddleware.port
+    );
 
-		this._socket = io(this._settingsMiddleware.hostname + ':' + this._settingsMiddleware.port);
+    this._socket.on("connect", () => {
+      this._socket.emit("endPointInformation", this._settingsEndpoint);
+    });
 
-		this._socket.on('connect', () => {
-			this._socket.emit('endPointInformation', this._settingsEndpoint);
-		});
+    // message received
+    this._socket.on("data", data => {
+      this._receiveMessage(data);
+    });
 
-		// message received
-		this._socket.on('data', (data) => {
-			this._receiveMessage(data);
-		});
+    this._socket.on("disconnect", function() {
+      console.log("disconnect now");
+      this._socket = null;
+    });
+  }
 
-		this._socket.on('disconnect', function(){
-			console.log("disconnect now");
-			this._socket.close();
-		});
-	}
-
-	sendMessage(data){
-		console.log(`send:\n\r${JSON.stringify(data, null, 2)}`);
-		this._socket.emit('sendData', data);
-	}
+  sendMessage(data) {
+    this._socket.emit("sendData", data);
+  }
 }
